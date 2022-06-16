7c63bcc6fa5dc86dea26a9bbe4b0da5c71c9e434
disc0v3ry <disc0v3ry@163.com>
2022-06-16 20:31:10 +0800

    first commit

---
+++ src/main/java/com/atdiscovery/GitTest.java
@@ -0,0 +1,16 @@
+package com.atdiscovery;
+
+/**
+ * @author Discoveryu
+ * @version 2022.04
+ * @date 2022/6/16 20:28
+ */
+public class GitTest {
+    public static void main(String[] args){
+        
+
+
+    }
+}
+
+
